5c4bf5abc57060760b9ed51982c10fb1580c9849
Jorge-Fonseca <jo68126@gmail.com>
2023-12-22 19:42:35 +0100

    projeto iniciado

---
+++ src/test/java/com/jorge/helpdesk/HeldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeldeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
